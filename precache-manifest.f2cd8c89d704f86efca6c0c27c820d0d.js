37d52514f03bb857a47378e9af70cc5ab7e01020
Mustafa Aleem <mustafa.aleem@reedbusiness.com>
2019-03-03 20:34:51 +0000

    Updates

---
+++ precache-manifest.f2cd8c89d704f86efca6c0c27c820d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "43e3d01482fef35f899c",
+    "url": "/robofriends/static/js/main.43e3d014.chunk.js"
+  },
+  {
+    "revision": "c13f14990d10f389ba2d",
+    "url": "/robofriends/static/js/2.c13f1499.chunk.js"
+  },
+  {
+    "revision": "43e3d01482fef35f899c",
+    "url": "/robofriends/static/css/main.d4bda858.chunk.css"
+  },
+  {
+    "revision": "c13f14990d10f389ba2d",
+    "url": "/robofriends/static/css/2.cd15141f.chunk.css"
+  },
+  {
+    "revision": "de962d8a0f58be7598069fd2235280cb",
+    "url": "/robofriends/index.html"
+  }
+];
